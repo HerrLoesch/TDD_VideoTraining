7fd65ba5604296341c30648e32e4040ba2bba0c1
Hendrik Lösch <hendrik.loesch@saxsys.de>
2020-11-18 18:53:16 +0100

    Initialized Fizz Buzz Solution

---
+++ FizzBuzz/FizzBuzzKata/FizzBuzzImplementation/FizzBuzzConverter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzConverter
+    {
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzKata/FizzBuzzKata/FizzBuzzConverterTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using FizzBuzzKata;
+
+namespace FizzBuzzKata.Tests
+{
+    /*
+     * 1 -> 1
+     * 2 -> 2
+     * 3 -> Fizz
+     * 4 -> 4
+     * 5 -> Buzz
+     * 6 -> Fizz
+     * 10 -> Buzz
+     * 15 -> FizzBuzz */
+    public class FizzBuzzConverterTests
+    {
+
+    }
+}
