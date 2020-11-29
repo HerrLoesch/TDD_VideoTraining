f48822e916d95711005215558d30df8e03527801
Hendrik Lösch <hendrik.loesch@saxsys.de>
2020-11-29 16:46:57 +0100

    Walking Skeleton

---
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.AcceptanceTests/PublicationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PublicationTool.AcceptanceTests
+{
+    public class PublicationTests
+    {
+        [Test]
+        public async Task Publication_service_can_be_reached()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:56985/Publication");
+
+            var result = await httpClient.GetAsync("");
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.Web/Controllers/PublicationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublicationTool.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PublicationController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>();
+        }
+    }
+}
