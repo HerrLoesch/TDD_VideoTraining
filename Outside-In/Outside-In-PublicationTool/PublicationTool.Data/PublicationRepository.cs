046ae310ae5be7942fe4ce4fbfe0d640c41251ac
Hendrik Lösch <hendrik.loesch@saxsys.de>
2020-12-01 21:49:13 +0100

    Added repository to controller

---
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.Data/PublicationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PublicationTool.Domain;
+using PublicationTool.Domain.Interfaces;
+
+namespace PublicationTool.Data
+{
+    public class PublicationRepository : IPublicationRepository
+    {
+        public void Save(Publication publication)
+        {
+            
+        }
+    }
+}
--- Outside-In/Outside-In-PublicationTool/PublicationTool.Web/Startup.cs
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.Web/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PublicationTool.Data;
+using PublicationTool.Domain.Interfaces;
 
 namespace PublicationTool.Web
 {
@@ -24,6 +26,7 @@ namespace PublicationTool.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IPublicationRepository, PublicationRepository>();
             services.AddControllers();
         }
 
