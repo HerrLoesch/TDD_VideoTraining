b28f918f6aa7d80fce07f8fbdf450c7f33b32d68
Hendrik Lösch <hendrik.loesch@saxsys.de>
2020-12-06 21:44:15 +0100

    Added first bindings

---
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.AcceptanceTests/PublicationBindings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Gherkin.Ast;
+using PublicationTool.Domain.Interfaces;
+using PublicationTool.Domain.Objects;
+using TechTalk.SpecFlow;
+
+namespace PublicationTool.AcceptanceTests
+{
+    [Binding]
+    public class PublicationBindings
+    {
+        private ScenarioContext scenarioContext;
+
+        public PublicationBindings(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        [When(@"the publication is stored")]
+        public async Task WhenThePublicationIsStored()
+        {
+            var publication = scenarioContext.Get<Publication>();
+            var serializedPublication = JsonSerializer.Serialize(publication);
+            var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:56985/") };
+            var publishedContent = new StringContent(serializedPublication, Encoding.UTF8, "application /json");
+
+            var resultMessage = await httpClient.PostAsync("Publication", publishedContent);
+            var resultContent = await resultMessage.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<Result>(resultContent);
+
+            scenarioContext.Set(resultMessage);
+            scenarioContext.Set(result);
+        }
+
+        [Given(@"is a publication with title ""(.*)"" published on ""(.*)""")]
+        public void GivenIsAPublicationWithTitlePublishedOn(string title, string date)
+        {
+            var publication = CreatePublication(title, date);
+            scenarioContext.Set(publication);
+        }
+
+        private static Publication CreatePublication(string title, string date)
+        {
+            var publication = new Publication();
+            publication.Title = title;
+            publication.Date = DateTime.Parse(date);
+            return publication;
+        }
+    }
+}
--- Outside-In/Outside-In-PublicationTool/PublicationTool.AcceptanceTests/SavePublication.feature
+++ Outside-In/Outside-In-PublicationTool/PublicationTool.AcceptanceTests/SavePublication.feature
@@ -3,13 +3,13 @@ As a user I want to add mandatory publication data so that I can save a basic pu
 
 Scenario: Add valid publication
 	Given is a publication with title "TDD Advanced" published on "22.06.2020"
-	When the save button is clicked
+	When the publication is stored
 	Then a publication "TDD Advanced" published on "22.06.2020" can be found in the data base
 	And no errors are reported
 
 Scenario Outline: Missing data
 	Given is a publication with title "<title>" published on "<publicationDate>"
-	When the save button is clicked
+	When the publication is stored
 	Then a publication "<title>" can not be found in the data base
 	And a missing "<propertyName>" is reported
 
@@ -20,7 +20,7 @@ Scenario Outline: Missing data
 
 Scenario Outline: wrong title
 	Given is a publication with title "<title>" published on "22.06.2020"
-	When the save button is clicked
+	When the publication is stored
 	Then a publication "<title>" can not be found in the data base
 	And it is reported that the title is to "<criteria>"
 
